0fb5755f0c2d62b481c85eeaacf71ceaeead8a27
Manoj Kumawat <imkumawatmanoj@gmail.com>
2022-01-16 16:46:49 +0530

    added dotenv config code

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 const express = require("express")
+const dotenv = require('dotenv')
 const bodyParser = require("body-parser")
 const mongoose = require('mongoose')
 const log = require('./log/log')
+const basicAuth = require('express-basic-auth')
 const swaggerUI = require('swagger-ui-express')
 const docs = require('./swagger.json')
 
 const app = express()
 
-const dbstr = 'mongodb://127.0.0.1/urlshortner'
+//const dbstr = process.env.DB_CONNECTION_STRING || 'mongodb://mongo:27017/infracloud'
+
+const dbstr = 'mongodb://127.0.0.1/infracloud'
 mongoose.connect(
     dbstr,
     { useUnifiedTopology: true, useNewUrlParser: true }
@@ -30,7 +34,10 @@ app.use((req, res, next) => {
     next()
 })
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(docs))
+app.use('/api-docs', basicAuth({
+    users: { Infracloud: "Infracloud@2022" },
+    challenge: true,
+}), swaggerUI.serve, swaggerUI.setup(docs))
 
 
 const api = require('./route/routes')
