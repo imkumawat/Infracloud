7e6c5e7070e7f825cf15bf1a086d92439765775a
Manoj Kumawat <imkumawatmanoj@gmail.com>
2022-01-16 14:18:45 +0530

    added memory cache machenism

--- src/api/getURL.js
+++ src/api/getURL.js
@@ -1,28 +1,59 @@
 const urls = require("../model/model")
 const log = require('../log/log')
+
+var cache = require('memory-cache');
+
+
 async function getURL(req, res) {
 
-    if (!req.params.code) {
-        log.info("Short url id not found")
-        return res.status(404).json({ message: 'No short url id found' })
+    try {
+
+        if (!req.params.code) {
+            log.info("Short url id not found")
+            return res.status(404).json({ message: 'No short url id found' })
+        }
+
+
+
+
+        var data
+
+        if (data = cache.get(req.params.code)) {
+            log.info("URL Found in Cache Memory")
+            log.info(data)
+
+            return res.redirect(`${data}`)
+
+        }
+
+        else {
+
+            let obj = {}
+            obj.ids = req.params.code
+            let url = await urls.findOne(obj, { _id: 0, __v: 0, ids: 0 })
+
+
+
+            if (url) {
+                log.info("URL Found in Database")
+                log.info(url)
+                cache.put(req.params.code, url.longurl);
+                return res.redirect(`${url.longurl}`)
+            }
+
+            else {
+
+                log.info("No short url found in Database")
+                return res.status(404).json({ message: 'Invalid Short URL' })
+            }
+
+        }
+
+
     }
-
-    let obj = {}
-    obj.ids = req.params.code
-    let url = await urls.findOne(obj, { _id: 0, __v: 0, ids: 0 })
-
-    if (url) {
-        log.info("URL Found")
-        log.info(url)
-
-        return res.redirect(`${url.longurl}`)
-        // return res.status(200).json({ message: `Shortened URL: ${url.shorturl}` })
+    catch (e) {
+        log.error(e)
+        return res.status(500).josn({ message: "Internal Server Error" })
     }
-
-    else {
-        log.info("No short url found")
-        return res.status(404).json({ message: 'Invalid Short URL' })
-    }
-
 }
 module.exports = getURL
